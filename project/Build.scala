727234e92288b95d0f75835752a76cd8c26d6446
Dale Wijnand <dalewijnand@powa.com>
2013-05-30 19:35:01 +0100

    Implement Todo list, with Heroku files too.

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,16 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "todolist"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "todolist"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
+    "org.postgresql" % "postgresql" % "9.2-1003-jdbc4",
     jdbc,
     anorm
   )
 
-
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
   )
-
 }
