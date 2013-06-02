3f3f2eec089f01ceb921347155430c02187d3ea5
Dale Wijnand <dalewijnand@powa.com>
2013-06-02 19:31:32 +0100

    Add bootstrap (2.3.2) via webjars (2.1.0-2).

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,10 @@ import play.Project._
 object Build extends Build {
 
   val dependencies = Seq(
+    "org.webjars" % "bootstrap" % "2.3.2",
     "com.google.guava" % "guava" % "14.0.1",
     "com.google.code.findbugs" % "jsr305" % "1.3.9", // Guava's provided dependency but seems scalac needs it
+    "org.webjars" %% "webjars-play" % "2.1.0-2",
     "com.google.zxing" % "javase" % "2.2"
   )
 
