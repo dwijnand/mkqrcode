0312a8453918ef2c69cfa61c3d8117cde54a9392
Dale Wijnand <dalewijnand@powa.com>
2013-06-04 16:03:15 +0100

    app/controllers/Application: Fully qualify & sort imports, & add visibility modifiers.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,30 +1,32 @@
 package controllers
 
-import collection.JavaConverters._
 
 import com.google.common.io.BaseEncoding
 import com.google.zxing.{EncodeHintType, BarcodeFormat}
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.qrcode.QRCodeWriter
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
-import models.Params
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
-import utils._
+
+import controllers.utils._
+import models.Params
+
+import scala.collection.JavaConverters._
 
 import java.io.ByteArrayOutputStream
 import java.util.Locale
 import javax.imageio.ImageIO
 
 object Application extends Controller {
-  val locale = Locale.ENGLISH
-  val qrCodeWriter = new QRCodeWriter
-  val encodingHints = Map(EncodeHintType.CHARACTER_SET -> "UTF-8")
+  private[this] val locale = Locale.ENGLISH
+  private[this] val qrCodeWriter = new QRCodeWriter
+  private[this] val encodingHints = Map(EncodeHintType.CHARACTER_SET -> "UTF-8")
 
-  val initialParams = Params("https://www.powatag.com/cps/1234567890", 100, uppercase = true, ErrorCorrectionLevel.L)
+  private[this] val initialParams = Params("https://www.powatag.com/cps/1234567890", 100, uppercase = true, ErrorCorrectionLevel.L)
 
-  val paramsForm = Form(
+  private[this] val paramsForm = Form(
     mapping(
       "contents" -> nonEmptyText,
       "size" -> number,
@@ -47,7 +49,7 @@ object Application extends Controller {
 
   def post = Action(request => Redirect(routes.Application.home().url, request.body.asFormUrlEncoded.get))
 
-  def render(p: Params) = {
+  private[this] def render(p: Params) = {
     val encodingHints = this.encodingHints + (EncodeHintType.ERROR_CORRECTION -> p.errorCorrectionLevel)
     val data = if (p.uppercase) p.contents.toUpperCase(locale) else p.contents
 
