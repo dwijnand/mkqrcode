10267a9a17b13243c46de2c3b3c52b463a941058
Dale Wijnand <dalewijnand@powa.com>
2013-06-04 16:56:29 +0100

    Minor renames.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -23,20 +23,20 @@ object Application extends Controller {
   private[this] val initialParams =
     Params("https://www.powatag.com/cps/1234567890", 100, uppercase = true, ErrorCorrectionLevel.L)
 
-  private[this] val paramsForm = Form(
+  private[this] val form = Form(
     mapping(
       "contents" -> nonEmptyText,
       "size" -> number,
       "uppercase" -> boolean,
-      "errorCorrectionLevel" -> enum[ErrorCorrectionLevel]
+      "ecLevel" -> enum[ErrorCorrectionLevel]
     )(Params.apply)(Params.unapply)
   )
 
   def home = Action {
     implicit request => {
       request.queryString.size match {
-        case 0 => Ok(views.html.Application.home(None, paramsForm.fill(initialParams)))
-        case _ => paramsForm.bindFromRequest.fold(
+        case 0 => Ok(views.html.Application.home(None, form.fill(initialParams)))
+        case _ => form.bindFromRequest.fold(
           formWithErrors => BadRequest(views.html.Application.home(None, formWithErrors)),
           params => Ok(render(params))
         )
@@ -47,7 +47,7 @@ object Application extends Controller {
   def post = Action(request => Redirect(routes.Application.home().url, request.body.asFormUrlEncoded.get))
 
   private[this] def render(p: Params) = {
-    val encodingHints = this.encodingHints + (EncodeHintType.ERROR_CORRECTION -> p.errorCorrectionLevel)
+    val encodingHints = this.encodingHints + (EncodeHintType.ERROR_CORRECTION -> p.ecLevel)
     val data = if (p.uppercase) p.contents.toUpperCase(locale) else p.contents
 
     val qrCode = QREncoder.encode(data, encodingHints)
@@ -58,6 +58,6 @@ object Application extends Controller {
     val bytes = byteArrayOutputStream.toByteArray
     val qrCodeString = BaseEncoding.base64().encode(bytes)
 
-    views.html.Application.home(Some((p, qrCodeString)), paramsForm.fill(p))
+    views.html.Application.home(Some((p, qrCodeString)), form.fill(p))
   }
 }
--- app/models/Params.scala
+++ app/models/Params.scala
@@ -2,4 +2,4 @@ package models
 
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 
-case class Params(contents: String, size: Int, uppercase: Boolean, errorCorrectionLevel: ErrorCorrectionLevel)
+case class Params(contents: String, size: Int, uppercase: Boolean, ecLevel: ErrorCorrectionLevel)
